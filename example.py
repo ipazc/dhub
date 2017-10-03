8664e1c4db46080b64658bc444344383f5b09c82
Iván <ipazce00@estudiantes.unileon.es>
2017-10-03 19:42:05 +0200

    *Added method to add all elements at once * Optimized smart updater, now it doesn't require close() in all the cases, so exceptions are better handled. * Added iterator with options compatible with mongo. * Added deletion in batches and clear() method.

--- example.py
+++ example.py
@@ -1,66 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from io import BytesIO
-import os
 from time import sleep
-from zipfile import ZipFile, ZIP_DEFLATED
+
 from mldata.datasets import Datasets
-from mldata.interpreters.bin_image import BinImage
 
 __author__ = 'Iván de Paz Centeno'
 
 
-token = "1b57b8a85bdc403b8d20eefd91a9f8e1"
-
-def remove_elements(dataset):
-    for id in dataset.keys(): del dataset[id]
+token = "edcc5b9cd94840559267f9c9f454d9b1"
 
 datasets = Datasets(token)
+
+"""
+element_kwargs = [
+    {'title':'a', 'description': 'a', 'tags': ['age: 5'], 'http_ref': "none", 'content': "hola{}".format(x).encode()} for x in range(50)
+] + [
+    {'title':'b', 'description': 'b', 'tags': ['age: 15'], 'http_ref': "none", 'content': "holb{}".format(x).encode()} for x in range(20)
+]
+"""
+
 dataset = datasets[0]
+#elements = dataset.add_elements(element_kwargs)
 
-#dataset[0].set_ref("ref1")
-#dataset[1].set_ref("ref2")
-dataset[2].set_content(b"holba2")
-sleep(5)
-dataset[2].set_ref("jebd")
-print(dataset[2])
-dataset.close()
+for element in dataset.filter_iter({'tags':{'$regex':'age[:][ ]?5'}}):
+    print(element)
+print(dataset)
+sleep(2)
 
-"""iteration = -1
-for element in dataset:
-    iteration += 1
-    content = element.get_content()
-    if content == b"a":
-        print(element)
-        print(dataset[5])
-        print("{}: {}".format(iteration, content))
-        break
-"""
+print(dataset[0])
+print(dataset[1])
 
+print(dataset[0].get_content())
+print(dataset[1].get_content())
 
-#elements = dataset[0:5]
-
-#for element in elements:
-#    print(element)
-#    print(element.get_content())
-"""dataset = datasets[0]
-
-element = dataset[0]
-
-content = element.get_content()
-
-with BytesIO() as b:
-    with ZipFile(b, mode="w", compression=ZIP_DEFLATED) as z:
-        z.writestr("data1", content)
-        z.writestr("data2", content)
-    b.seek(0)
-    zipped_content = b.read()
-
-print(len(zipped_content))
-
-with BytesIO(zipped_content) as b, ZipFile(b) as z:
-    result = z.read("data2")
-
-bin_image = BinImage()
-bin_image.cipher(result).show()
-"""
+#dataset.close()
+#element = dataset.add_element("title1", "desc1", ["hi", "ho"], "none", b"hola")
+#dataset.close()
--- mldata/wrapper/smart_updater.py
+++ mldata/wrapper/smart_updater.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import concurrent
 from concurrent.futures import ThreadPoolExecutor
 from queue import Queue
 import threading
@@ -18,9 +19,11 @@ class AsyncSmartUpdater(object):
         self.api_wrapper_owner = api_wrapper_owner
         self.content_put_queue = Queue()
         self.element_update_queue = Queue()
+        self.queues_priorities = [self.element_update_queue, self.content_put_queue]
         self.__exit = False
+        self.__cancel_pending_jobs = False
         self.lock = threading.Lock()
-        self.thread = threading.Thread(target=self._thread_func)
+        self.thread = threading.Thread(target=self._thread_func, daemon=True)
         self.thread.start()
 
     @property
@@ -34,33 +37,69 @@ class AsyncSmartUpdater(object):
         with self.lock:
             self.__exit = do_exit
 
+    @property
+    def _cancel_pending_jobs(self):
+        with self.lock:
+            cancel_pending_jobs = self.__cancel_pending_jobs
+        return cancel_pending_jobs
+
+    @_cancel_pending_jobs.setter
+    def _cancel_pending_jobs(self, do__cancel_pending_jobs=True):
+        with self.lock:
+            self.__cancel_pending_jobs = do__cancel_pending_jobs
+
+    def __queue_with_higher_priority_waiting(self, queue):
+        """
+        Checks if is there any queue with higher priority waiting than the speicified queue.
+        :param queue: queue to check from
+        :return: boolean True if there is at least one queue with higher priority waiting. False otherwise.
+        """
+        index = 0
+        current_queue_index = self.queues_priorities.index(queue)
+
+        while index < current_queue_index and self.queues_priorities[index].qsize() == 0:
+            index += 1
+
+        return index != len(self.queues_priorities) and index < current_queue_index
+
+    def __any_queue_with_elements(self):
+        return any([queue.qsize() > 0 for queue in self.queues_priorities])
+
     def _thread_func(self):
         ps = self.server_info['Page-Size']
-        queues = {'binary':[self.content_put_queue], 'json':[self.element_update_queue]}
+        queue_types = {self.content_put_queue:'binary', self.element_update_queue: 'json'}
 
-        while not self._exit:
-
+        while not self._exit or (not self._cancel_pending_jobs and self.__any_queue_with_elements()):
+            print("I'm here... {} {} {}".format(self._exit, self._cancel_pending_jobs, self.__any_queue_with_elements()))
             # Let's gather all the updates from the queue to serialize them
             sleep(UPDATE_INTERVAL)
 
-            for request_kind, queues_list in queues.items():
+            for queue in self.queues_priorities:
 
-                for queue in queues_list:
+                if self.__queue_with_higher_priority_waiting(queue):
+                    print("HERE???")
+                    break # This forces the loop to start running through the most priority queues again.
 
-                    gathered_elements = []
+                gathered_elements = []
 
-                    while queue.qsize() > 0:
-                        gathered_elements.append(queue.get())
+                futures = []
+                while queue.qsize() > 0:
 
-                        # This is the smart action: we combine several requests into one
-                        if len(gathered_elements) > ps:
-                            pool.submit(self.__do_update, request_kind, gathered_elements)
-                            gathered_elements = []
+                    # Check that queues with highest priority are not
+                    if self.__queue_with_higher_priority_waiting(queue):
+                        break # This forces the loop to start running through the most priority queues again.
 
-                    if len(gathered_elements) > 0:
-                        pool.submit(self.__do_update, request_kind, gathered_elements)
+                    gathered_elements.append(queue.get())
 
+                    # This is the smart action: we combine several requests into one
+                    if len(gathered_elements) > ps:
+                        futures.append(pool.submit(self.__do_update, queue_types[queue], gathered_elements))
+                        gathered_elements = []
 
+                if len(gathered_elements) > 0:
+                    futures.append(pool.submit(self.__do_update, queue_types[queue], gathered_elements))
+
+                concurrent.futures.wait(futures)
 
     def __do_update(self, request_kind, elements):
 
@@ -81,12 +120,15 @@ class AsyncSmartUpdater(object):
         else: # request_kind == "binary":
             self.api_wrapper_owner._put_binary(url, extra_data=None, binary=PyZip(kwargs_list).to_bytes())
 
+        return True
+
     def queue_update(self, url, element_id, kwargs):
         self.element_update_queue.put([url, element_id, kwargs])
 
     def queue_content_update(self, url, element_id, content):
         self.content_put_queue.put([url, element_id, content])
 
-    def stop(self):
+    def stop(self, cancel_pending_jobs=False):
         self.__exit = True
+        self.__cancel_pending_jobs = cancel_pending_jobs
         self.thread.join(10)
